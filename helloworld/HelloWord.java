478e1fed043ac117af22c55e978991dd149c6532
haririwardhana <haririwardhana@student.telkomuniversity.ac.id>
2020-09-17 14:10:36 +0700

    HelloWord.java

---
+++ helloworld/HelloWord.java
@@ -0,0 +1,8 @@
+public class HelloWord{
+		
+		public static void main(String[] args)	{
+			System.out.println("nim = 6706200044");
+			System.out.println("nama = Hariri Wardhana");
+			System.out.println("kelas = D3-IF-44-03");
+		}
+	}
